0f8345a4e06324353821a5070041902edd7667eb
safiul-incrementsinc <safiul.islam@incrementsinc.com>
2023-11-20 16:39:21 +0600

    revert: build runner cmd

--- bin/ipp_starter_kit.dart
+++ bin/ipp_starter_kit.dart
@@ -1,13 +1,11 @@
 import 'package:args/command_runner.dart';
-import 'package:ipp_starter_kit/commands/build_runner.dart';
 import 'package:ipp_starter_kit/commands/change_app_name.dart';
 import 'package:ipp_starter_kit/commands/create.dart';
 
 Future<void> main(List<String> args) async {
   final runner = CommandRunner('ipp', 'Official CLI for IPP xD')
     ..addCommand(CreateCommand())
-    ..addCommand(ChangeAppNameCommand())
-    ..addCommand(BuildRunnerCommand());
+    ..addCommand(ChangeAppNameCommand());
 
   await runner.run(args);
 }
